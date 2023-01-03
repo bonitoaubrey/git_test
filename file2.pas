fb6395b36c10b0078e592c9307cfe78dd97422d4
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-01-03 13:30:58 +0300

    remove all files and make 4 new files

---
+++ file2.pas
@@ -0,0 +1,81 @@
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
---
+++ file3.pas
@@ -0,0 +1,6 @@
+program hello2;
+
+begin
+    writeln('Hello, World!');
+    writeln('Good bye, World!');
+end.
